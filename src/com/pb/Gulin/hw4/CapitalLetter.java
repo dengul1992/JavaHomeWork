2148c1bfd941245ad2d0e5407ba0b0a89af03535
Denis Gulin <gulindenis1992@gmail.com>
2021-10-30 22:54:04 +0300

    init commit

---
+++ src/com/pb/Gulin/hw4/CapitalLetter.java
@@ -0,0 +1,26 @@
+package com.pb.Gulin.hw4;
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class CapitalLetter {
+    public static String firstUpperCase(String word){
+        if(word == null || word.isEmpty()) return ""; //или return word;
+        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    }
+    public static void main(String[] args) {
+
+
+        Scanner scan = new Scanner(System.in);
+        String w;
+        System.out.println("Введите предложение и я переведу все первые буквы слов в верхний регистр) ...");
+        w= scan.next();
+        w=firstUpperCase(w);
+        System.out.println(w);
+
+        while (){
+
+        }
+    }
+    }
+
