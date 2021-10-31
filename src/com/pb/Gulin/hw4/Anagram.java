3e80f53187a619c8cdd1270d10313d9286f7e328
Denis Gulin <gulindenis1992@gmail.com>
2021-10-31 15:04:05 +0200

    init commit

--- src/com/pb/Gulin/hw4/Anagram.java
+++ src/com/pb/Gulin/hw4/Anagram.java
@@ -1,29 +1,52 @@
 package com.pb.Gulin.hw4;
 import java.util.Scanner;
-import java.util.Arrays;
-
 
 
 public class Anagram {
-    public static String firstUpperCase(String word){
-        if(word == null || word.isEmpty()) return ""; //или return word;
-        return word.substring(0, 1).toUpperCase() + word.substring(1);
+    public static String wd(String line) {
+        String LowerLine=line.toLowerCase();
+        return LowerLine.replaceAll("[^A-Za-zА-Яа-я0-9]", "");
     }
+
+    public static String[] ToLowerArray(String line){
+        String LowerLine=wd(line);
+        return LowerLine.split("");
+    }
+
+
+
     public static void main(String[] args) {
 
-
         Scanner scan = new Scanner(System.in);
-        String w;
-        System.out.println("Введите предложение и я переведу все первые буквы слов в верхний регистр) ...");
-        w= scan.next();
-        w=firstUpperCase(w);
-        System.out.println(w);
+        String FirstLine, SecondLine;
+        String [] FirstLineAr, SecondLineAr;
+        boolean find=true;
+        System.out.println("Тут мы проверим  будут ли 2 предложения анаграммой друг другу");
 
-        while (){
+        System.out.println("Введите пожалуйста 1-е предложение");
+        FirstLine= scan.nextLine();
+        FirstLineAr=ToLowerArray(FirstLine);
 
+        System.out.println("Введите пожалуйста 2-е предложение");
+        SecondLine= scan.nextLine();
+        SecondLineAr=ToLowerArray(SecondLine);
+if (FirstLineAr.length==SecondLineAr.length){
+
+        String search=wd(FirstLine);
+
+        for (int i=0; i<FirstLineAr.length; i++) {
+            if (!search.contains(SecondLineAr[i])){
+                find = false;
+                break;
+            }
         }
-    }
+        if (find) {
+            System.out.println("Ура!) Вторая строка является анаграммой первой");
+        } else {
+            System.out.println("Нет, нет, нет - это не анаграмма");
+        }
+} else {
+    System.out.println("Нет, нет, нет - это не анаграмма");
 }
-
-
+    }
 }
